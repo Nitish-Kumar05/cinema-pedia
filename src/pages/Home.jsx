be30d806900ef2f41868c7487b2cfaa97fb34610
Nitish-Kumar05 <mvnitishkumar@gmail.com>
2023-08-21 16:41:40 +0530

    added search box and made API requests

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,29 @@
-import { Link } from "react-router-dom"
+import { useState } from "react"
 
 const Home = () => {
+
+    const [searchStr, setSearchStr] = useState('')
+
+    const onSearchInputChange = (e) => {
+        setSearchStr(e.target.value);
+    }
+
+    const onSearch = async (e) => {
+        e.preventDefault();
+        // For API request
+        const response = await fetch(`https://api.tvmaze.com/search/shows?q=${searchStr}`);
+        const data = await response.json();
+        console.log(data)
+    }
+
     return (
         <>
-            Home
+            <form onSubmit={onSearch} >
+                <input type='text' onChange={onSearchInputChange} />
+                <button type="submit" value={searchStr} onChange={onSearchInputChange} >
+                    Search
+                </button>
+            </form>
         </>
     )
 }
